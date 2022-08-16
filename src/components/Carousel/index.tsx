e1f5bf5052e978a8f63ee6ae7a53efc1d6a47b24
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-17 01:02:57 +0200

    UPDT add api request

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -17,47 +17,14 @@ interface CarouselProps {
 
 export function Carousel() {
   const [continents, setContinents] = useState<CarouselProps[]>([]);
-  useEffect(() => {
-    const continentsTemp: CarouselProps[] = [
-      {
-        slug: "europa",
-        title: "Europa",
-        slogan: "O continente mais antigo.",
-        image: "/banner/europe.jpeg",
-      },
-      {
-        slug: "africa",
-        title: "Africa",
-        slogan: "O continente selvagem.",
-        image: "/banner/africa.jpeg",
-      },
-      {
-        slug: "america-norte",
-        title: "America Norte",
-        slogan: "O continente dos sonhos.",
-        image: "/banner/america-nort.jpeg",
-      },
-      {
-        slug: "america-sul",
-        title: "America Sul",
-        slogan: "O continente da diversão.",
-        image: "/banner/america-sul.jpeg",
-      },
-      {
-        slug: "asia",
-        title: "Asia",
-        slogan: "O continente das culturas.",
-        image: "/banner/asia.jpeg",
-      },
-      {
-        slug: "oceania",
-        title: "Oceania",
-        slogan: "O continente marítimo.",
-        image: "/banner/oceania.jpeg",
-      },
-    ];
 
-    setContinents(continentsTemp);
+  useEffect(() => {
+    async function getContinents() {
+      await fetch(`http://localhost:3000/api/getContinents`)
+        .then((data) => data.json())
+        .then(setContinents);
+    }
+    getContinents();
   }, []);
 
   return (
@@ -70,45 +37,46 @@ export function Carousel() {
         keyboard={true}
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
       >
-        {continents.map((continent) => (
-          <SwiperSlide key={continent.slug}>
-            <VStack
-              w="100%"
-              h="450px"
-              alignItems="center"
-              justifyContent="center"
-              background={`linear-gradient(0deg, rgba(28, 20, 1, 0.35), rgba(28, 20, 1, 0.35)), url('${continent.image}') center`}
-            >
-              <Link href={`/${continent.slug}`}>
-                <ChakraLink
-                  textAlign="center"
-                  transition="filter .2s"
-                  _hover={{
-                    textDecoration: "none",
-                    filter: "brightness(.8)",
-                  }}
-                >
-                  <Text
-                    fontWeight="700"
-                    fontSize="48px"
-                    lineHeight="72px"
-                    color="brand.gray-50"
+        {continents &&
+          continents.map((continent) => (
+            <SwiperSlide key={continent.slug}>
+              <VStack
+                w="100%"
+                h="450px"
+                alignItems="center"
+                justifyContent="center"
+                background={`linear-gradient(0deg, rgba(28, 20, 1, 0.35), rgba(28, 20, 1, 0.35)), url('${continent.image}') center`}
+              >
+                <Link href={`/${continent.slug}`}>
+                  <ChakraLink
+                    textAlign="center"
+                    transition="filter .2s"
+                    _hover={{
+                      textDecoration: "none",
+                      filter: "brightness(.8)",
+                    }}
                   >
-                    {continent.title}
-                  </Text>
-                  <Text
-                    fontWeight="700"
-                    fontSize="24px"
-                    lineHeight="36px"
-                    color="brand.gray-300"
-                  >
-                    {continent.slogan}
-                  </Text>
-                </ChakraLink>
-              </Link>
-            </VStack>
-          </SwiperSlide>
-        ))}
+                    <Text
+                      fontWeight="700"
+                      fontSize="48px"
+                      lineHeight="72px"
+                      color="brand.gray-50"
+                    >
+                      {continent.title}
+                    </Text>
+                    <Text
+                      fontWeight="700"
+                      fontSize="24px"
+                      lineHeight="36px"
+                      color="brand.gray-300"
+                    >
+                      {continent.slogan}
+                    </Text>
+                  </ChakraLink>
+                </Link>
+              </VStack>
+            </SwiperSlide>
+          ))}
       </Swiper>
     </Box>
   );
